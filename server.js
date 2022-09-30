45ee272137aee9c4cf180dd8fd45024b44f22e6e
Kasudhananurag <114092146+Kasudhananurag@users.noreply.github.com>
2022-09-30 21:15:17 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,4 +6,12 @@ app.get("/",function(req,res){
   res.send("Hello");
 });
 
+app.get("/about",function(req,res){
+  res.send("Trying to learn node, this is just  a small part");
+});
+
+app.get("/contact",function(req,res){
+  res.send("contact me here at my phone number 989898989 and or @ contact@gmail.com");
+});
+
 app.listen(3000,function(){console.log("Server is running on the port 3000")});
