484cd25bd60189eb66c2ddf9e2e947b7f33f81ac
Kasudhananurag <114092146+Kasudhananurag@users.noreply.github.com>
2022-09-30 21:18:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -14,4 +14,8 @@ app.get("/contact",function(req,res){
   res.send("contact me here at my phone number 989898989 and or @ contact@gmail.com");
 });
 
+app.get("/hobbies",function(req,res){
+  res.send("i love watching you and taling to you");
+});
+
 app.listen(3000,function(){console.log("Server is running on the port 3000")});
